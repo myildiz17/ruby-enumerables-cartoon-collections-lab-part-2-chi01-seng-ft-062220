49068f091e3e2d4a5264b2ea43371424611e1465
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-06-10 05:26:15 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,15 +1,26 @@
-planeteer_calls=["a","b","c","d"]
-set=["t","k","c"]
-  #p planeteer_calls.include?("a")
-count=0
-while count< set.size do
-  if planeteer_calls.include? (set[count])
-    p set[count]
-    return set[count]
+bands = {
+  joy_division: %w[ian bernard peter stephen],
+  the_smiths: %w[johnny andy morrissey mike],
+  the_cramps: %w[lux ivy nick],
+  blondie: %w[debbie chris clem jimmy nigel],
+  talking_heads: %w[david tina chris jerry]
+}
 
-  end
-    count+=1
+firstmost_name = bands.reduce(nil) do |memo, (key, value)|
+  # On the first pass, we don't have a name, so just grab the first one.
+  memo = value[0] if !memo
+
+  # Sort that array of names
+  sorted_names = value.sort
+
+  # If string comparison says the sorted name of the array is earlier than
+  # the memo, it becomes the new memo.
+  memo = sorted_names[0] if sorted_names[0] <= memo
+
+  # Return the memo as per reduce rules
+  # (Try adding 'p' in front of memo to see how it changes as the enumerate the
+  # pair of the hash!)
+  memo
 end
-
-
-#puts " #{i}"  if [1, 2, 11].any? { |i| (1..9).include?(i) }
+p firstmost_name
+ 
---
+++ test2.rb
@@ -0,0 +1,3 @@
+a=[2,4,7,9]
+puts a.map { |e| e*e }
+puts a
